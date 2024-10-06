9e78e814c962fb2364067865678426125e77d145
Goutham Kumar <goutham@outlook.com>
2024-10-06 15:58:01 +0000

    restore file to previous working condition

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
 import requests
 
-# Make a GET request to a public API
 response = requests.get("https:/api.github.com/users/github")
 
-# Check if the request was successful
 if response.status_code == 200:
-  data.response.json()
-
-  print(f"Username: {data['login']}")
+  data = response.json()
