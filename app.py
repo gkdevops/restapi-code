1a3bf643a551d003d048619328050f20db95d97c
Goutham Kumar <goutham@outlook.com>
2024-10-06 16:37:45 +0000

    Added comments to the code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import requests
 
+# Make a GET request to a public GitHub API
 response = requests.get("https:/api.github.com/users/github")
 
+# Check if the response is successful
 if response.status_code == 200:
   data = response.json()
