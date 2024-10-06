db2631362ab4c5ac7c30d8bcbaf5a8058401add0
Goutham Kumar <goutham@outlook.com>
2024-10-06 15:51:31 +0000

    Added print statement to the code

--- app.py
+++ app.py
@@ -5,4 +5,6 @@ response = requests.get("https:/api.github.com/users/github")
 
 # Check if the request was successful
 if response.status_code == 200:
-  data = response.json()
+  data.response.json()
+
+  print(f"Username: {data['login']}")
